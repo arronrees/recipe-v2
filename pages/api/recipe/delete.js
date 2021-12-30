892164296073b50cf604b36ae2b7874101b67c3c
arronrees <64908445+arronrees@users.noreply.github.com>
2021-12-30 11:00:37 +0000

    Added delete saved recipes when recipe deleted

--- pages/api/recipe/delete.js
+++ pages/api/recipe/delete.js
@@ -1,9 +1,11 @@
 import { Recipe } from '../../../utils/db/models/Recipe';
+import { SavedRecipe } from '../../../utils/db/models/SavedRecipe';
 
 export default async function deleteRecipe(req, res) {
   const { id } = req.query;
 
   const recipe = await Recipe.destroy({ where: { id } });
+  const savedRecipes = await SavedRecipe.destroy({ where: { recipeId: id } });
 
   res.json({ message: 'Recipe deleted successfully' });
   return;
