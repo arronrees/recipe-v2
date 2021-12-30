a1c81c6fc399f8f7e868fe2db841786ec2474ffd
arronrees <64908445+arronrees@users.noreply.github.com>
2021-12-30 16:21:20 +0000

    Deleted recipe category link when deleting recipe

--- pages/api/recipe/delete.js
+++ pages/api/recipe/delete.js
@@ -1,11 +1,15 @@
 import { Recipe } from '../../../utils/db/models/Recipe';
 import { SavedRecipe } from '../../../utils/db/models/SavedRecipe';
+import { RecipeCategory } from '../../../utils/db/models/RecipeCategory';
 
 export default async function deleteRecipe(req, res) {
   const { id } = req.query;
 
   const recipe = await Recipe.destroy({ where: { id } });
   const savedRecipes = await SavedRecipe.destroy({ where: { recipeId: id } });
+  const recipeCategories = await RecipeCategory.destroy({
+    where: { recipeId: id },
+  });
 
   res.json({ message: 'Recipe deleted successfully' });
   return;
